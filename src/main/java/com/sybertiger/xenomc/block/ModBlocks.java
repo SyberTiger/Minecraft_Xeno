3cfcba5aad2d717be1f29f77e2e7cdd10aac0905
SyberTiger <SyberTiger.studios@gmx.de>
2021-12-03 21:47:09 +0100

    added silicon blocks

---
+++ src/main/java/com/sybertiger/xenomc/block/ModBlocks.java
@@ -0,0 +1,54 @@
+package com.sybertiger.xenomc.block;
+
+import com.sybertiger.xenomc.XenoMC;
+import com.sybertiger.xenomc.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.OreBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, XenoMC.MOD_ID);
+
+
+    public static final RegistryObject<Block> SILICON_BLOCK = registerBlock("silicon_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.METAL).requiresCorrectToolForDrops().strength(3.0f, 6.0f)));
+
+    public static final RegistryObject<Block> RAW_SILICON_BLOCK = registerBlock("raw_silicon_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.METAL).requiresCorrectToolForDrops().strength(3.0f, 6.0f)));
+
+    public static final RegistryObject<Block> SILICON_ORE = registerBlock("silicon_ore",
+            () -> new OreBlock(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.STONE).requiresCorrectToolForDrops().strength(3.0f, 3.0f)));
+
+    public static final RegistryObject<Block> DEEPSLATE_SILICON_ORE = registerBlock("deepslate_silicon_ore",
+            () -> new Block(BlockBehaviour.Properties.copy(SILICON_ORE.get()).color(MaterialColor.DEEPSLATE).strength(4.5f, 3.0f).sound(SoundType.DEEPSLATE)));
+
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block){
+        RegistryObject<T> toReturn = BLOCKS.register(name,block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block){
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
+    }
+
+    public static void register(IEventBus eventBus){
+        BLOCKS.register(eventBus);
+    }
+
+}
